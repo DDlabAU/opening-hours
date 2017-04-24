8f8225ff3762b7048dee516fff76a9556d3f1f72
NBAKH <hr.nbakh@gmail.com>
2017-04-24 18:16:05 +0200

    better text

--- sketch.js
+++ sketch.js
@@ -2,11 +2,18 @@ var openData;
 var openNext = [];
 var min;
 var fr = 6;
-var tSize = screen.height/20;
+var tSize;//window.height/20;
 
 function setup() {
   loadJSON('https://script.googleusercontent.com/macros/echo?user_content_key=ZOk4S3iYBmLoCSVEYTVn6jZX3Fz-Y0oDKxVEMBBz0hsaTlJgxjhypDtgykm4fEIynfmyFfA0uyg2c44x2MHVCTuVgThqk23hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDRtrHJ_B07pTY_RGQB9NgIlvQFo4AEb7AWNFWyMdXAla4PE5KW5lkb1btx4KUrnXmlgLNiGRXNx&lib=MLZpv_I1oO55YzaOvgVD5WaTNt6qxM66-', getData);
-  createCanvas(screen.width,screen.height);
+  //createCanvas(windowWidth ,windowHeight);
+  createCanvas (windowWidth, windowHeight);
+  if(windowWidth<windowHeight){
+    tSize = windowWidth/20;
+  }
+  else{
+    tSize = windowHeight/20;
+  }
   textSize(32);
   textAlign(CENTER);
   frameRate(fr);
@@ -15,6 +22,16 @@ function setup() {
 
 function update(){
   loadJSON('https://script.googleusercontent.com/macros/echo?user_content_key=ZOk4S3iYBmLoCSVEYTVn6jZX3Fz-Y0oDKxVEMBBz0hsaTlJgxjhypDtgykm4fEIynfmyFfA0uyg2c44x2MHVCTuVgThqk23hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDRtrHJ_B07pTY_RGQB9NgIlvQFo4AEb7AWNFWyMdXAla4PE5KW5lkb1btx4KUrnXmlgLNiGRXNx&lib=MLZpv_I1oO55YzaOvgVD5WaTNt6qxM66-', getData);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  if(windowWidth<windowHeight){
+    tSize = windowWidth/20;
+  }
+  else{
+    tSize = windowHeight/20;
+  }
 }
 
 function getData(data) {
@@ -26,20 +43,20 @@ function draw() {
     background(255);
     var result = handleData();
     if(result == 'Niels' || result == 'Ann' || result == 'Nikolaj'){
-      text("DD Lab er åbent",screen.width/2, screen.height/2-tSize);
-      text(result + ' er på arbejde',screen.width/2, screen.height/2);
+      text("DD Lab er åbent",windowWidth/2, windowHeight/2-tSize);
+      text(result + ' er på arbejde',windowWidth/2, windowHeight/2);
       var closingHours = openData.items[openNext[0]].end.dateTime.substr(11,5);
-      text("Lukker kl " + closingHours, screen.width/2, screen.height/2+tSize);
+      text("Lukker kl " + closingHours, windowWidth/2, windowHeight/2+tSize);
 
       text("Der er også åbent d. " + openData.items[openNext[1]].start.dateTime.substr(8,2) +
        "/" + openData.items[openNext[1]].start.dateTime.substr(5,2) +
         ", fra kl " +openData.items[openNext[1]].start.dateTime.substr(11,5) + " til "
         +openData.items[openNext[1]].end.dateTime.substr(11,5)
-        ,screen.width/2, screen.height/2+tSize*3);
+        ,windowWidth/2, windowHeight/2+tSize*3);
     }
     else{
         var textPlacement = -6*tSize;
-        text('DD Lab er lukket',screen.width/2, screen.height/2+textPlacement);
+        text('DD Lab er lukket',windowWidth/2, windowHeight/2+textPlacement);
         textPlacement=textPlacement+tSize;
         for(var i = 0; i<3; i++){
           textPlacement=textPlacement+tSize;
@@ -47,9 +64,9 @@ function draw() {
            "/" + openData.items[openNext[i]].start.dateTime.substr(5,2) +
             ", fra kl " +openData.items[openNext[i]].start.dateTime.substr(11,5) + " til "
             +openData.items[openNext[i]].end.dateTime.substr(11,5)
-              ,screen.width/2, screen.height/2+textPlacement);
+              ,windowWidth/2, windowHeight/2+textPlacement);
           textPlacement=textPlacement+tSize;
-          text("ansatte på arbejde er "+ openData.items[openNext[i]].summary,screen.width/2, screen.height/2+textPlacement);
+          text("ansatte på arbejde er "+ openData.items[openNext[i]].summary,windowWidth/2, windowHeight/2+textPlacement);
           textPlacement=textPlacement+tSize;
         }
       }
