d62d2f4bda1a70d958a9d9c4bf27a9307c4837cb
RaggedyAnn <rikignirrak@hotmail.com>
2017-04-25 16:00:44 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,12 @@ var openNext = [];
 var min;
 var fr = 6;
 var tSize;//window.height/20;
+var ddImg;
+
+function preload () {
+  ddImg=loadImage("lab1.png");
+}
+
 
 function setup() {
   loadJSON('https://script.googleusercontent.com/macros/echo?user_content_key=ZOk4S3iYBmLoCSVEYTVn6jZX3Fz-Y0oDKxVEMBBz0hsaTlJgxjhypDtgykm4fEIynfmyFfA0uyg2c44x2MHVCTuVgThqk23hm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDRtrHJ_B07pTY_RGQB9NgIlvQFo4AEb7AWNFWyMdXAla4PE5KW5lkb1btx4KUrnXmlgLNiGRXNx&lib=MLZpv_I1oO55YzaOvgVD5WaTNt6qxM66-', getData);
@@ -42,7 +48,11 @@ function getData(data) {
 function draw() {
   if(openData){
     background(50);
-    fill(225);
+    imageMode(CENTER);
+    ddImg.resize(0,width/8);
+    image(ddImg,width/2, height/5.5);
+    filter(BLUR,3);
+    fill(255);
     var result = handleData();
     if(result == 'Niels' || result == 'Ann' || result == 'Nikolaj'){
       text("DD Lab er ",windowWidth/2-textWidth("åbent")/2, windowHeight/2-tSize-tSize/2);
@@ -51,19 +61,23 @@ function draw() {
       text("åbent",windowWidth/2+textWidth("DD Lab er ")-textWidth("åbent")/2, windowHeight/2-tSize-tSize/2);
       textAlign(CENTER);
       fill(255);
-      //text("DD Lab er åbent",windowWidth/2, windowHeight/2-tSize);
       text(result + ' er på arbejde',windowWidth/2, windowHeight/2);
       var closingHours = openData.items[openNext[0]].end.dateTime.substr(11,5);
-      text("Lukker kl " + closingHours, windowWidth/2, windowHeight/2+tSize);
-      text("Der er også åbent d. " + openData.items[openNext[1]].start.dateTime.substr(8,2) +
+      text("Lukker kl " + closingHours, windowWidth/2, windowHeight/2+tSize+tSize/2);
+
+      text("Næste åbningstid d. " + openData.items[openNext[1]].start.dateTime.substr(8,2) +
        "/" + openData.items[openNext[1]].start.dateTime.substr(5,2) +
         ", fra kl " +openData.items[openNext[1]].start.dateTime.substr(11,5) + " til "
         +openData.items[openNext[1]].end.dateTime.substr(11,5)
-        ,windowWidth/2, windowHeight/2+tSize*3);
+        ,windowWidth/2, windowHeight/2+tSize*4);
     }
     else{
         var textPlacement = -6*tSize;
-        text('DD Lab er lukket',windowWidth/2, windowHeight/2+textPlacement);
+        text('DD Lab er ',windowWidth/2-textWidth("lukket")/2, windowHeight/2+textPlacement);
+        fill(255,0,0);
+        textAlign(RIGHT);
+        text('lukket',windowWidth/2+textWidth("DD Lab er ")-textWidth("lukket")/2, windowHeight/2+textPlacement);
+        fill(255);
         textPlacement=textPlacement+tSize;
         for(var i = 0; i<3; i++){
           textPlacement=textPlacement+tSize;
@@ -118,3 +132,4 @@ function drawOpen(){
     text(dates[i].summary + ': ' + dates[i].start.dateTime, w,h+20);
   }
 }
+
