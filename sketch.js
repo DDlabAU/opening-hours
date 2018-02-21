077fbd2a762c2277498f69418dc38dd2054357c9
NBAKH <hr.nbakh@gmail.com>
2018-02-21 14:58:56 +0100

    fix image placement

--- sketch.js
+++ sketch.js
@@ -86,7 +86,7 @@ function basicBG(){
   textSize(tSize/2);
   // textAlign(RIGHT);
   // text("ddlab logo placeholder", windowWidth-2 ,tSize/2);
-  image(ddImg, windowWidth-windowWidth/10, height/2, windowWidth/10, windowWidth/10);
+  image(ddImg, windowWidth-windowWidth/10-10, 10, windowWidth/10, windowWidth/10);
   textAlign(CENTER);
   textSize(tSize);
   text("Åbningstider: Man: 10-17, Tirs: 9-16, Ons: 10-17, fre: 9-16:00", windowWidth/2,windowHeight-tSize);
